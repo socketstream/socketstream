986cf22133911529fa140bcfb5872d7348060fa6
Roman Minkin <minkinroman@gmail.com>
2013-10-24 13:07:09 -0400

    1. Fixed description for function chDir() in lib/utils/file.js 2. Added description for functions and public methods in lib/utils/file.js 3. Updated public method .readDirSync() in lib/utils/file.js, removed unnecessary variables and IF statements 4. Added tests and fixtures for .readDirSync()

--- test/unit/utils/file.test.js
+++ test/unit/utils/file.test.js
@@ -1,12 +1,15 @@
+/*jshint immed: false */
+
 'use strict';
 
-var fs         = require('fs'),
-    path       = require('path'),
-    should     = require('should'),
-    ac         = require('../../helpers/assertionCounter'),
-    fileUtils  = require( path.join(process.env.PWD, 'lib/utils/file') ),
-    projectDir = path.join(process.env.PWD, 'test/fixtures/project'),
-    testDir    = path.join(process.env.PWD, 'test/fixtures/files');
+var fs              = require('fs'),
+    path            = require('path'),
+    should          = require('should'),
+    ac              = require('../../helpers/assertionCounter'),
+    fileUtils       = require( path.join(process.env.PWD, 'lib/utils/file') ),
+    projectDir      = path.join(process.env.PWD, 'test/fixtures/project'),
+    testDir         = path.join(process.env.PWD, 'test/fixtures/files'),
+    testReadDirSync = path.join(process.env.PWD, 'test/fixtures/ReadDirSync');
 
 describe('lib/utils/file', function () {
     describe('.isDir()', function () {
@@ -37,6 +40,81 @@ describe('lib/utils/file', function () {
 
     });
 
+
+    describe('.readDirSync()', function () {
+
+        // Used to reset the counters before each test
+        beforeEach(ac.reset);
+
+        it('should read directory in recursive mode', function (done) {
+            var found;
+
+            ac.expect(23);
+
+            found = fileUtils.readDirSync(testReadDirSync);
+
+            found.should.be.an.instanceOf(Object).andCheck();
+            found.should.have.properties('files', 'dirs').andCheck();
+
+            found.files.should.be.an.instanceOf(Array).andCheck();
+            found.dirs.should.be.an.instanceOf(Array).andCheck();
+
+            found.should.have.property('files').with.lengthOf(6).andCheck();
+            found.should.have.property('dirs').with.lengthOf(10).andCheck();
+
+            /* check all the subdirectories */
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir1') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir2') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.1') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.2') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.2', 'dir3.2.1') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.2', 'dir3.2.1') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3', 'dir3.3.1') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3', 'dir3.3.2') ).andCheck();
+            found.dirs.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3', 'dir3.3.3') ).andCheck();
+
+            /* check all the files in subdirectories */
+            found.files.should.includeEql( path.join(testReadDirSync, 'index.js') ).andCheck();
+            found.files.should.includeEql( path.join(testReadDirSync, 'dir2', 'index.html') ).andCheck();
+            found.files.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.2', 'dir3.2.1', 'test.js') ).andCheck();
+            found.files.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3', 'test.css') ).andCheck();
+            found.files.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3', 'dir3.3.2', 'test.csv') ).andCheck();
+            found.files.should.includeEql( path.join(testReadDirSync, 'dir3', 'dir3.3', 'dir3.3.3', 'test.sh') ).andCheck();
+
+            ac.check(done);
+        });
+
+        it('should not throw an error if path is missing (does not exist)', function (done) {
+            var start = path.join(testReadDirSync, 'unexisting_directory');
+
+            ac.expect(0);
+
+            (function() {
+                fileUtils.readDirSync( start );
+
+            }).should.not.throw();
+
+            ac.check(done);
+
+        });
+
+        it('should throw an error if path exists, but not a directory', function (done) {
+            var start = path.join(testReadDirSync, 'dir2', 'index.html');
+
+            ac.expect(0);
+
+            (function() {
+                fileUtils.readDirSync( start );
+
+            }).should.throw("path: " + start + " is not a directory");
+
+            ac.check(done);
+
+        })
+
+    });
 
     describe('.findExtForBasePath()', function () {
 
