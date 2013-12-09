16a989c9211b1deffafc3a0c6e62abf7b7195b6c
Roman Minkin <minkinroman@gmail.com>
2013-12-09 16:21:55 -0500

    2nd phase of linting/testing 'lib/http' - added tests for .load() method - added mock for connect, so http requests could be tested w/o loading an actial ss.http instance - added helper for extracting functions name

--- test/unit/http/index.test.js
+++ test/unit/http/index.test.js
@@ -1,21 +1,52 @@
 'use strict';
 
+/**
+ * Using mock for connect.
+ * Mock includes additional methods for testing as .get(), .end(), etc.
+ * To be able to check http respond right away
+ *
+ * Example:
+ *
+ *     http.middleware.request().get('/favicon.ico').end(function(res) {
+ *         res.statusCode.should.equal(200);
+ *         ac.check(done);
+ *     });
+ *
+ */
+require('../../helpers/connect');
+
 var path           = require('path'),
     connect        = require('connect'),
     httpFunc       = require( path.join(process.env.PWD, 'lib/http/index') ),
     http           = null,
     app            = null,
     ac             = require('../../helpers/assertionCounter'),
+    utils          = require('../../helpers/utils.js'),
     sessionStore   = new connect.session.MemoryStore,
+    settings       = {        // User-configurable settings with sensible defaults
+      "static": {
+        maxAge: 2592000000    // (30 * 24 * 60 * 60 * 1000) cache static assets in the browser for 30 days
+      },
+      secure: false           // allow setting of the 'secure' cookie attribute when using SSL - see https://github.com/socketstream/socketstream/issues/349
+    },
     sessionOptions = {
         maxAge: 2592000000
     },
-    root           = path.join(process.cwd(), 'test', 'fixtures', 'project'); // replace '\' with '/' to support Windows
+    root           = path.join(process.cwd(), 'test/fixtures/project'); // replace '\' with '/' to support Windows
+
+function testPrependMiddleware(req, res, next) {
+    next();
+}
+
+function testAppendMiddleware(req, res, next) {
+    if (req) {
+        next();
+    }
+}
 
 describe('lib/http/index', function () {
 
     beforeEach(function (done) {
-
         ac.reset();
         http = null;
         app  = null;
@@ -23,7 +54,7 @@ describe('lib/http/index', function () {
     });
 
     describe('module.exports', function () {
-        it('should return a http function instance', function (done) {
+        it('should return a {Function} instance', function (done) {
             ac.expect(1);
 
             httpFunc.should.be.type('function').andCheck();
@@ -32,7 +63,7 @@ describe('lib/http/index', function () {
     });
 
     describe('module.exports()', function () {
-        it('should return http instance', function (done) {
+        it('should return http {Object} instance', function (done) {
             ac.expect(3);
 
             http = httpFunc(root);
@@ -41,31 +72,139 @@ describe('lib/http/index', function () {
             http.load.should.be.type('function').andCheck();
             http.route.should.be.type('function').andCheck();
 
-// console.dir(http.load('client/static', sessionStore, sessionOptions));
-
             ac.check(done);
         });
     });
 
-    describe('.load()', function () {
-        it('should append SocketStream middleware to stack', function (done) {
-            ac.expect(0);
+    describe('.set()', function () {
+        it('should set newSettings into private settings object');
+    });
 
+    describe('.route()', function () {
+        it('should add event listener for the url');
+    });
+
+    describe('.load()', function () {
+        var staticPath = '';
+
+        function setUp(done) {
+            staticPath = 'client/static';
+
+            /* Loading http function */
             http = httpFunc(root);
 
-            // console.log('http', http);
+            /* adding some test middlewars */
+            http.middleware.prepend(testPrependMiddleware);
+            http.middleware.append(testAppendMiddleware);
 
-            app = http.load('client/static', sessionStore, sessionOptions);
+            /* loading http server assets */
+            app = http.load(staticPath, sessionStore, sessionOptions);
 
-            // console.log('app', http.request);
-            // http.middleware.request().get('/favicon.ico').should.equla('1');
+            done();
+        }
 
-            // http.set.should.be.type('function').andCheck();
-            // http.load.should.be.type('function').andCheck();
-            // http.route.should.be.type('function').andCheck();
+        describe('itself', function () {
+            beforeEach(setUp);
 
+            /**
+             * connect.compress() should be added to middleware stack on highest possible position
+             */
+            it('should build propper middleware stack', function (done) {
+                ac.expect(18);
 
-            ac.check(done);
+                /* testing correct length of http.middleware.stack */
+                http.middleware.stack.should.be.an.instanceOf(Array).andCheck();
+                http.middleware.stack.should.have.length(8).andCheck();
+
+                /**
+                 * testing testPrependMiddleware
+                 * lib/http/index.js:104
+                 */
+                http.middleware.stack[0].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[0].handle.toString().should.equal( testPrependMiddleware.toString() ).andCheck();
+
+                /**
+                 * testing connect.compress()
+                 * It should be added to middleware stack on highest possible position
+                 * lib/http/index.js:142
+                 */
+                http.middleware.stack[1].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[1].handle.toString().should.equal( connect.compress().toString() ).andCheck();
+
+                /**
+                 * testing connect.cookieParser('SocketStream')
+                 * lib/http/index.js:145
+                 */
+                http.middleware.stack[2].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[2].handle.toString().should.equal( connect.cookieParser('SocketStream').toString() ).andCheck();
+
+                /**
+                 * testing connect.favicon()
+                 * lib/http/index.js:145
+                 */
+                http.middleware.stack[3].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[3].handle.toString().should.equal( connect.favicon( staticPath + '/favicon.ico').toString() ).andCheck();
+
+                /**
+                 * testing connect.session()
+                 * lib/http/index.js:145
+                 */
+                http.middleware.stack[4].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[4].handle.toString().should.equal( connect.session({
+                    cookie: {
+                        path: '/',
+                        httpOnly: false,
+                        maxAge: sessionOptions.maxAge,
+                        secure: settings.secure
+                    },
+                    store: sessionStore
+                }).toString() ).andCheck();
+
+                /**
+                 * testing testAppendMiddleware
+                 * lib/http/index.js:156
+                 */
+                http.middleware.stack[5].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[5].handle.toString().should.equal( testAppendMiddleware.toString() ).andCheck();
+
+                /**
+                 * testing eventMiddleware
+                 * since there ra no way to get the instance of eventMiddleware function,
+                 * just compare the functions names
+                 * lib/http/index.js:161
+                 */
+                http.middleware.stack[6].handle.should.be.an.instanceOf(Function).andCheck();
+                utils.getfunctionName( http.middleware.stack[6].handle ).should.equal( 'eventMiddleware' ).andCheck();
+
+                /**
+                 * testing connect["static"]()
+                 * lib/http/index.js:161
+                 */
+                http.middleware.stack[7].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[7].handle.toString().should.equal( connect["static"](staticPath, settings["static"]).toString() ).andCheck();
+
+                ac.check(done);
+            });
+        })
+
+        describe('return object', function () {
+            beforeEach(setUp);
+
+            it('should an instance Object', function (done) {
+                ac.expect(1);
+                http.should.be.an.instanceOf(Object).andCheck();
+                ac.check(done);
+            });
+
+            it('should respond for \'/favicon.ico\' request', function (done) {
+                ac.expect(1);
+
+                http.middleware.request().get('/favicon.ico').end(function(res) {
+                    res.statusCode.should.equal(200).andCheck();
+                    ac.check(done);
+                });
+            });
         });
+
     });
 });
