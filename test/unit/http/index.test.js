8e9e1181f7eb8e8231892f94b8743ffa85e58104
Roman Minkin <minkinroman@gmail.com>
2014-01-01 19:51:21 -0500

    - Added tests for lib/http/router.js - Minor changes for test/unit/http/index.test.js

--- test/unit/http/index.test.js
+++ test/unit/http/index.test.js
@@ -45,7 +45,7 @@ describe('lib/http/index', function () {
     });
 
     describe('module.exports', function () {
-        it('should return a {Function} instance', function (done) {
+        it('should return a function instance', function (done) {
             ac.expect(1);
 
             httpFunc.should.be.type('function').andCheck();
@@ -54,242 +54,244 @@ describe('lib/http/index', function () {
     });
 
     describe('module.exports()', function () {
-        it('should return http {Object} instance', function (done) {
-            ac.expect(3);
+        it('should return an http object instance', function (done) {
+            ac.expect(4);
+            http.should.be.type('object').with.properties('set', 'load', 'route').andCheck();
             http.set.should.be.type('function').andCheck();
             http.load.should.be.type('function').andCheck();
             http.route.should.be.type('function').andCheck();
 
             ac.check(done);
         });
-    });
 
-    describe('.set()', function () {
-        beforeEach(function (done) {
-          http = httpFunc(root);
-          done();
-        });
+        describe('#set()', function () {
+            beforeEach(function (done) {
+              http = httpFunc(root);
+              done();
+            });
 
-        it('should set newSettings into the private settings object', function (done) {
-            var newSettings = {
-                secure: true
-            };
+            it('should set newSettings into the private settings object', function (done) {
+                var newSettings = {
+                    secure: true
+                };
 
-            ac.expect(0);
-            /* Should now throw an error */
-            http.set(newSettings);
-            ac.check(done);
-        });
-
-        it('should throw an error if passed argument is not an object', function (done) {
-            var newSettings = '';
-
-            ac.expect(0);
-
-            /* Should throw an error */
-            (function() {
+                ac.expect(0);
+                /* Should now throw an error */
                 http.set(newSettings);
-            }).should.throw('ss.http.set() takes an object e.g. {static: {maxAge: 60000}}');
-
-            ac.check(done);
-        });
-    });
-
-    describe('.route()', function () {
-        var obj;
-
-        beforeEach(function(done) {
-            obj  = null;
-            http = httpFunc(root);
-            done();
-        });
-
-        it('should add event listener for given url', function (done) {
-            function callback(req, res) {
-                res.serve('main');
-            }
-
-            ac.expect(6);
-
-            obj = http.route('/', callback);
-
-            obj.should.be.an.instanceOf(Object).andCheck();
-            obj._events.should.be.an.instanceOf(Object).andCheck();
-            obj.listenerTree.should.be.an.instanceOf(Object).andCheck();
-            obj.listenerTree['/'].should.be.an.instanceOf(Object).andCheck();
-            obj.listenerTree['/']._listeners.should.be.an.instanceOf(Object).andCheck();
-            obj.listenerTree['/']._listeners.toString().should.include(callback.toString()).andCheck();
-
-            ac.check(done);
-        });
-
-        it('should add default event listener for the url with out callback', function (done) {
-            var router,
-                url,
-                callback = function(name) {
-                    return router.on(url, function(req, res) {
-                        return res.serveClient(name);
-                    });
-                }
-
-            ac.expect(3);
-
-            obj = http.route('/');
-
-            obj.should.be.an.instanceOf(Object).andCheck();
-            obj.serveClient.should.be.an.instanceOf(Object).andCheck();
-            obj.serveClient.toString().replace(/(\s+|\n)/g, ' ').should.include(callback.toString().replace(/(\s+|\n)/g, ' ')).andCheck();
-
-            ac.check(done);
-        });
-
-        it('should throw an error if does not start with \'/\'', function (done) {
-            var url = 'some_url_wwith_no_slash_at_the_start';
-
-            function callback(req, res) {
-                res.serve('main');
-            }
-
-            ac.expect(0);
-
-            (function() {
-                http.route(url, callback);
-
-            }).should.throw( util.format('%s is not a valid URL. Valid URLs must start with /', url) );
-
-
-            ac.check(done);
-        });
-    });
-
-    describe('.load()', function () {
-        var staticPath = '';
-
-        function setUp(done) {
-            staticPath = 'client/static';
-
-            /* Loading http function */
-            http = httpFunc(root);
-
-            /* adding some test middlewars */
-            http.middleware.prepend(testPrependMiddleware);
-            http.middleware.append(testAppendMiddleware);
-
-            /* loading http server assets */
-            app = http.load(staticPath, sessionStore, sessionOptions);
-
-            done();
-        }
-
-        function testPrependMiddleware(req, res, next) {
-            next();
-        }
-
-        function testAppendMiddleware(req, res, next) {
-            if (req) {
-                next();
-            }
-        }
-
-        beforeEach(setUp);
-
-        /**
-         * connect.compress() should be added to middleware stack on highest possible position
-         */
-        it('should build propper middleware stack', function (done) {
-            ac.expect(18);
-
-            /* testing correct length of http.middleware.stack */
-            http.middleware.stack.should.be.an.instanceOf(Array).andCheck();
-            http.middleware.stack.should.have.length(8).andCheck();
-
-            /**
-             * testing testPrependMiddleware
-             * lib/http/index.js:104
-             */
-            http.middleware.stack[0].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[0].handle.toString().should.equal( testPrependMiddleware.toString() ).andCheck();
-
-            /**
-             * testing connect.compress()
-             * It should be added to middleware stack on highest possible position
-             * lib/http/index.js:142
-             */
-            http.middleware.stack[1].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[1].handle.toString().should.equal( connect.compress().toString() ).andCheck();
-
-            /**
-             * testing connect.cookieParser('SocketStream')
-             * lib/http/index.js:145
-             */
-            http.middleware.stack[2].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[2].handle.toString().should.equal( connect.cookieParser('SocketStream').toString() ).andCheck();
-
-            /**
-             * testing connect.favicon()
-             * lib/http/index.js:145
-             */
-            http.middleware.stack[3].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[3].handle.toString().should.equal( connect.favicon( staticPath + '/favicon.ico').toString() ).andCheck();
-
-            /**
-             * testing connect.session()
-             * lib/http/index.js:145
-             */
-            http.middleware.stack[4].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[4].handle.toString().should.equal( connect.session({
-                cookie: {
-                    path: '/',
-                    httpOnly: false,
-                    maxAge: sessionOptions.maxAge,
-                    secure: settings.secure
-                },
-                store: sessionStore
-            }).toString() ).andCheck();
-
-            /**
-             * testing testAppendMiddleware
-             * lib/http/index.js:156
-             */
-            http.middleware.stack[5].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[5].handle.toString().should.equal( testAppendMiddleware.toString() ).andCheck();
-
-            /**
-             * testing eventMiddleware
-             * since there ra no way to get the instance of eventMiddleware function,
-             * just compare the functions names
-             * lib/http/index.js:161
-             */
-            http.middleware.stack[6].handle.should.be.an.instanceOf(Function).andCheck();
-            utils.getfunctionName( http.middleware.stack[6].handle ).should.equal( 'eventMiddleware' ).andCheck();
-
-            /**
-             * testing connect["static"]()
-             * lib/http/index.js:161
-             */
-            http.middleware.stack[7].handle.should.be.an.instanceOf(Function).andCheck();
-            http.middleware.stack[7].handle.toString().should.equal( connect["static"](staticPath, settings["static"]).toString() ).andCheck();
-
-            ac.check(done);
-        });
-
-        describe('returned app/http object', function () {
-            beforeEach(setUp);
-
-            it('should be an instance of Object', function (done) {
-                ac.expect(1);
-                app.should.be.an.instanceOf(Object).andCheck();
                 ac.check(done);
             });
 
-            it('should respond with status 200 for /favicon.ico request', function (done) {
-                ac.expect(1);
+            it('should throw an error if passed argument is not an object', function (done) {
+                var newSettings = '';
 
-                request(app).get('/favicon.ico').end(function(err, res) {
-                    res.statusCode.should.equal(200).andCheck();
+                ac.expect(0);
+
+                /* Should throw an error */
+                (function() {
+                    http.set(newSettings);
+                }).should.throw('ss.http.set() takes an object e.g. {static: {maxAge: 60000}}');
+
+                ac.check(done);
+            });
+        });
+
+        describe('#route()', function () {
+            var obj;
+
+            beforeEach(function(done) {
+                obj  = null;
+                http = httpFunc(root);
+                done();
+            });
+
+            it('should add event listener for given url', function (done) {
+                function callback(req, res) {
+                    res.serve('main');
+                }
+
+                ac.expect(6);
+
+                obj = http.route('/', callback);
+
+                obj.should.be.an.instanceOf(Object).andCheck();
+                obj._events.should.be.an.instanceOf(Object).andCheck();
+                obj.listenerTree.should.be.an.instanceOf(Object).andCheck();
+                obj.listenerTree['/'].should.be.an.instanceOf(Object).andCheck();
+                obj.listenerTree['/']._listeners.should.be.an.instanceOf(Object).andCheck();
+                obj.listenerTree['/']._listeners.toString().should.include(callback.toString()).andCheck();
+
+                ac.check(done);
+            });
+
+            it('should add default event listener for the url with out callback', function (done) {
+                var router,
+                    url,
+                    callback = function(name) {
+                        return router.on(url, function(req, res) {
+                            return res.serveClient(name);
+                        });
+                    }
+
+                ac.expect(3);
+
+                obj = http.route('/');
+
+                obj.should.be.an.instanceOf(Object).andCheck();
+                obj.serveClient.should.be.an.instanceOf(Object).andCheck();
+                obj.serveClient.toString().replace(/(\s+|\n)/g, ' ').should.include(callback.toString().replace(/(\s+|\n)/g, ' ')).andCheck();
+
+                ac.check(done);
+            });
+
+            it('should throw an error if does not start with \'/\'', function (done) {
+                var url = 'some_url_wwith_no_slash_at_the_start';
+
+                function callback(req, res) {
+                    res.serve('main');
+                }
+
+                ac.expect(0);
+
+                (function() {
+                    http.route(url, callback);
+
+                }).should.throw( util.format('%s is not a valid URL. Valid URLs must start with /', url) );
+
+
+                ac.check(done);
+            });
+        });
+
+        describe('#load()', function () {
+            var staticPath = '';
+
+            function setUp(done) {
+                staticPath = 'client/static';
+
+                /* Loading http function */
+                http = httpFunc(root);
+
+                /* adding some test middlewars */
+                http.middleware.prepend(testPrependMiddleware);
+                http.middleware.append(testAppendMiddleware);
+
+                /* loading http server assets */
+                app = http.load(staticPath, sessionStore, sessionOptions);
+
+                done();
+            }
+
+            function testPrependMiddleware(req, res, next) {
+                next();
+            }
+
+            function testAppendMiddleware(req, res, next) {
+                if (req) {
+                    next();
+                }
+            }
+
+            beforeEach(setUp);
+
+            /**
+             * connect.compress() should be added to middleware stack on highest possible position
+             */
+            it('should build propper middleware stack', function (done) {
+                ac.expect(18);
+
+                /* testing correct length of http.middleware.stack */
+                http.middleware.stack.should.be.an.instanceOf(Array).andCheck();
+                http.middleware.stack.should.have.length(8).andCheck();
+
+                /**
+                 * testing testPrependMiddleware
+                 * lib/http/index.js:104
+                 */
+                http.middleware.stack[0].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[0].handle.toString().should.equal( testPrependMiddleware.toString() ).andCheck();
+
+                /**
+                 * testing connect.compress()
+                 * It should be added to middleware stack on highest possible position
+                 * lib/http/index.js:142
+                 */
+                http.middleware.stack[1].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[1].handle.toString().should.equal( connect.compress().toString() ).andCheck();
+
+                /**
+                 * testing connect.cookieParser('SocketStream')
+                 * lib/http/index.js:145
+                 */
+                http.middleware.stack[2].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[2].handle.toString().should.equal( connect.cookieParser('SocketStream').toString() ).andCheck();
+
+                /**
+                 * testing connect.favicon()
+                 * lib/http/index.js:145
+                 */
+                http.middleware.stack[3].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[3].handle.toString().should.equal( connect.favicon( staticPath + '/favicon.ico').toString() ).andCheck();
+
+                /**
+                 * testing connect.session()
+                 * lib/http/index.js:145
+                 */
+                http.middleware.stack[4].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[4].handle.toString().should.equal( connect.session({
+                    cookie: {
+                        path: '/',
+                        httpOnly: false,
+                        maxAge: sessionOptions.maxAge,
+                        secure: settings.secure
+                    },
+                    store: sessionStore
+                }).toString() ).andCheck();
+
+                /**
+                 * testing testAppendMiddleware
+                 * lib/http/index.js:156
+                 */
+                http.middleware.stack[5].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[5].handle.toString().should.equal( testAppendMiddleware.toString() ).andCheck();
+
+                /**
+                 * testing eventMiddleware
+                 * since there ra no way to get the instance of eventMiddleware function,
+                 * just compare the functions names
+                 * lib/http/index.js:161
+                 */
+                http.middleware.stack[6].handle.should.be.an.instanceOf(Function).andCheck();
+                utils.getfunctionName( http.middleware.stack[6].handle ).should.equal( 'eventMiddleware' ).andCheck();
+
+                /**
+                 * testing connect["static"]()
+                 * lib/http/index.js:161
+                 */
+                http.middleware.stack[7].handle.should.be.an.instanceOf(Function).andCheck();
+                http.middleware.stack[7].handle.toString().should.equal( connect["static"](staticPath, settings["static"]).toString() ).andCheck();
+
+                ac.check(done);
+            });
+
+            describe('returned app/http object', function () {
+                beforeEach(setUp);
+
+                it('should be an instance of Object', function (done) {
+                    ac.expect(1);
+                    app.should.be.an.instanceOf(Object).andCheck();
                     ac.check(done);
+                });
+
+                it('should respond with status 200 for /favicon.ico request', function (done) {
+                    ac.expect(1);
+
+                    request(app).get('/favicon.ico').end(function(err, res) {
+                        res.statusCode.should.equal(200).andCheck();
+                        ac.check(done);
+                    });
                 });
             });
         });
     });
+
 });
--- test/unit/http/router.test.js
+++ test/unit/http/router.test.js
@@ -1,25 +1,200 @@
+/*jshint immed: false */
+
 'use strict';
 
-var path   = require('path'),
-    ac     = require('../../helpers/assertionCounter.js'),
-    Router = require( path.join(process.env.PWD, 'lib/http/router.js') ).Router;
+var path          = require('path'),
+    EventEmitter2 = require('eventemitter2').EventEmitter2,
+    ac            = require('../../helpers/assertionCounter.js'),
+    Router        = require( path.join(process.env.PWD, 'lib/http/router.js') ).Router,
+    req           = {},
+    res           = {},
+    cb,
+    router;
 
 describe('lib/http/router', function () {
 
-    it('should exports Router as a function', function (done) {
+    beforeEach(function (done) {
+        router = new (Router);
+
+        cb = function() {
+            return;
+        }
+
+        done();
+    });
+
+    it('should exports `Router` as a function', function (done) {
         ac.expect(1);
 
         Router.should.be.type('function').andCheck();
         ac.check(done);
     });
 
-    describe('Router', function () {
-        describe('.route(url, req, res)', function () {
-            it('.route');
+    describe('Router()', function () {
+
+        it('should be an object', function (done) {
+            ac.expect(1);
+            router.should.be.type('object').andCheck();
+            ac.check(done);
+        });
+
+        it('should have an object property #ee', function (done) {
+            ac.expect(1);
+            router.should.have.a.property('ee').with.a.type('object').andCheck();
+            ac.check(done);
+        });
+
+        it('should have a function property #on', function (done) {
+            ac.expect(1);
+            router.should.have.a.property('on').with.a.type('function').andCheck();
+            ac.check(done);
+        });
+
+        it('should have a function property #route', function (done) {
+            ac.expect(1);
+            router.should.have.a.property('route').with.a.type('function').andCheck();
+            ac.check(done);
+        });
+
+        describe('Router#ee', function () {
+            it('should be an instance of EventEmitter2', function (done) {
+                ac.expect(1);
+                router.ee.should.be.an.instanceOf(EventEmitter2).andCheck();
+                ac.check(done);
+            });
+
+            it('should have an empty object property #_events', function (done) {
+                ac.expect(2);
+                router.ee.should.have.a.property('_events').with.a.type('object').andCheck();
+                router.ee._events.should.be.empty.andCheck();
+                ac.check(done);
+            });
+
+            it('should have a string property #delimiter = ?', function (done) {
+                ac.expect(1);
+                router.ee.should.have.a.property('delimiter', '?').with.a.type('string').andCheck();
+                ac.check(done);
+            });
+
+            it('should have a boolean property #wildcard = true', function (done) {
+                ac.expect(1);
+                router.ee.should.have.a.property('wildcard', true).with.a.type('boolean').andCheck();
+                ac.check(done);
+            });
+
+            it('should have an empty object property #listenerTree', function (done) {
+                ac.expect(2);
+                router.ee.should.have.a.property('listenerTree').with.a.type('object').andCheck();
+                router.ee.listenerTree.should.be.empty.andCheck();
+                ac.check(done);
+            });
+        });
+
+        describe('Router#route(url, req, res)', function () {
+            it('should return false for `/` if there are no routes and no listners', function (done) {
+                ac.expect(1);
+                router.route('/', req, res).should.be.false.andCheck();
+                ac.check(done);
+            });
+
+            it('should return true for `/` if there is a listner for `/`', function (done) {
+                ac.expect(1);
+
+                router.on('/', cb);
+                router.route('/', req, res).should.be.true.andCheck();
+                ac.check(done);
+            });
+
+            it('should return false for `/` if there is a listner for `/404`', function (done) {
+                ac.expect(1);
+
+                router.on('/404', cb);
+                router.route('/', req, res).should.be.false.andCheck();
+                ac.check(done);
+            });
+
+            it('should return false for `/200` if there is a listner for `/404`', function (done) {
+                ac.expect(1);
+
+                router.on('/404', cb);
+                router.route('/200', req, res).should.be.false.andCheck();
+                ac.check(done);
+            });
+
+            it('should return false for an empty url', function (done) {
+                ac.expect(1);
+
+                router.on('/404', cb);
+                router.route('', req, res).should.be.false.andCheck();
+                ac.check(done);
+            });
+
+            it('should support Push State Routing', function (done) {
+                ac.expect(5);
+
+                router.on('/', cb);
+
+                router.route('/1', req, res).should.be.true.andCheck();
+                router.route('/1/', req, res).should.be.true.andCheck();
+                router.route('/1/2', req, res).should.be.true.andCheck();
+                router.route('/1/2/3', req, res).should.be.true.andCheck();
+                router.route('/1/2/3/4', req, res).should.be.true.andCheck();
+                ac.check(done);
+            });
+
+            it('should return true if the url request contains parameters', function (done) {
+                ac.expect(2);
+
+                router.on('/some-url', cb);
+                router.route('/some-url?ts=12345', req, res).should.be.true.andCheck();
+                router.route('/some-url/?ts=12345', req, res).should.be.true.andCheck();
+                ac.check(done);
+            });
+
+            /**
+             * test for #286 Static file error with 404
+             * https://github.com/socketstream/socketstream/pull/419
+             */
+            it('should return false for static file and true for Push State Routing', function (done) {
+                ac.expect(5);
+
+                router.on('/', cb);
+
+                router.route('/content/uploads/345.jpg', req, res).should.be.false.andCheck();
+                router.route('/content/uploads/', req, res).should.be.true.andCheck();
+                router.route('/content', req, res).should.be.true.andCheck();
+                router.route('/', req, res).should.be.true.andCheck();
+                router.route('', req, res).should.be.true.andCheck();
+                ac.check(done);
+            });
+        });
+
+        describe('Router#on(url, cb)', function () {
+            it('should throw an error if url does not start with `/`', function (done) {
+                var url = 'some-url';
+
+                (function(){
+                    router.on(url, cb)
+                }).should.throw(url + ' is not a valid URL. Valid URLs must start with /');
+
+                ac.check(done);
+            });
+
+            it('should not throw an error if url does start with `/`', function (done) {
+                var url = '/some-url';
+
+                (function(){
+                    router.on(url, cb)
+                }).should.not.throw(url + ' is not a valid URL. Valid URLs must start with /');
+
+                ac.check(done);
+            });
+
+            it('should return an instance of EventEmitter2 for correct url', function (done) {
+                ac.expect(1);
+                router.on('/some-url', cb).should.be.an.instanceOf(EventEmitter2).andCheck();
+                ac.check(done);
+            });
         });
     });
-
 });
-
-
-console.log('router', Router);
