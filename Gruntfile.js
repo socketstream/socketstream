6e2a0b68286213d61973dc7f7e15de52790e57c9
Roman Minkin <rminkin@vaynermedia.com>
2014-01-21 12:18:19 -0500

    Added tasks for updating docs in gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,14 +163,80 @@ module.exports = function(grunt) {
             options: {
                 reporter: 'spec'
             }
-        }
+        },
+        shell: {
+            //We use %version% and evluate it at run-time, because <%= pkg.version %>
+            //is only evaluated once
+            'release-prepare': [
+                'grunt before-test after-test',
+                'grunt version', //remove "-SNAPSHOT"
+                'grunt changelog'
+            ],
+            'release-complete': [
+                'git commit CHANGELOG.md package.json -m "chore(release): v%version%"',
+                'git tag %version%'
+            ],
+            'release-start': [
+                'grunt version:minor:"SNAPSHOT"',
+                'git commit package.json -m "chore(release): Starting v%version%"'
+            ],
+            'pre-update-gh-pages': [
+                'git checkout gh-pages',
+                'git merge master'
+            ],
+            'post-update-gh-pages': [
+                'git commit docs -m "Updating docs: v%version%"',
+                'git checkout master'
+            ]
+        },
     });
 
-    //Rename our watch task to 'delta', then make actual 'watch'
+    // Rename our watch task to 'delta', then make actual 'watch'
     grunt.renameTask('watch', 'delta');
+
+    function setVersion(type, suffix) {
+        var file = 'package.json',
+            VERSION_REGEX = /([\'|\"]version[\'|\"][ ]*:[ ]*[\'|\"])([\d|.]*)(-\w+)*([\'|\"])/,
+            contents = grunt.file.read(file),
+            version;
+        contents = contents.replace(VERSION_REGEX, function(match, left, center) {
+            version = center;
+            if (type) {
+                version = require('semver').inc(version, type);
+            }
+            //semver.inc strips our suffix if it existed
+            if (suffix) {
+                version += '-' + suffix;
+            }
+            return left + version + '"';
+        });
+        grunt.log.ok('Version set to ' + version.cyan);
+        grunt.file.write(file, contents);
+        return version;
+    }
+
+    grunt.registerTask('version', 'Set version. If no arguments, it just takes off suffix', function() {
+        setVersion(this.args[0], this.args[1]);
+    });
+
+    grunt.registerMultiTask('shell', 'run shell commands', function() {
+        var self = this,
+            sh = require('shelljs');
+        self.data.forEach(function(cmd) {
+            cmd = cmd.replace('%version%', grunt.file.readJSON('package.json').version);
+            grunt.log.ok(cmd);
+            var result = sh.exec(cmd, {
+                silent: true
+            });
+            if (result.code !== 0) {
+                grunt.fatal(result.output);
+            }
+        });
+    });
 
     grunt.registerTask('default', 'Default task which runs all the required subtasks', ['jshint', 'test']);
     grunt.registerTask('test', 'Test everything', ['mochaTest']);
-    grunt.registerTask('build:docs', 'Build documentation', ['clean', 'ngdocs']);
-    grunt.registerTask('watch:docs', 'Watching for changes and re-building documentation', ['concurrent:docsSite']);
+    grunt.registerTask('build:docs', 'Build documentation', ['clean:docs', 'ngdocs']);
+    grunt.registerTask('watch:docs', 'Watching for changes and re-building docs', ['concurrent:docsSite']);
+    grunt.registerTask('update:docs', 'Update gh-page branch from master', ['clean:docs', 'shell:pre-update-gh-pages', 'build:docs', 'shell:post-update-gh-pages']);
 }
