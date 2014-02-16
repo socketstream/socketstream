b659a63d8ed8bb3c003a80ec2fa830b8c9e72501
Paul Jensen <paulbjensen@gmail.com>
2014-02-16 06:51:38 +0000

    test(lib): Fleshed out one of the tests for lib/socketstream.js

--- test/unit/socketstream.test.js
+++ test/unit/socketstream.test.js
@@ -2,7 +2,7 @@
 
 
 
-/* Dependencies */
+// Dependencies
 
 var ac            = require('../helpers/assertionCounter');
 
@@ -14,7 +14,17 @@ describe('lib/socketstream', function () {
 
     beforeEach(function (done) {
 
+        process.env.NODE_ENV = undefined;
         ac.reset();
+        done();
+
+    });
+
+
+
+    afterEach(function (done) {
+
+        process.env.NODE_ENV = 'test';
         done();
 
     });
@@ -33,7 +43,12 @@ describe('lib/socketstream', function () {
 
 
 
-        it('should default to development, if neither NODE_ENV or SS_ENV are passed');
+        it('should default to development, if neither NODE_ENV or SS_ENV are passed', function (done) {
+            ac.expect(1);
+            var socketstream = require('../../lib/socketstream.js');
+            socketstream.env.should.equal('development').andCheck();
+            ac.check(done);
+        });
 
 
 
