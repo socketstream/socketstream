fedf8ac3a561a02cc032a7f537c1200b9b3905f7
Paul Jensen <paulbjensen@gmail.com>
2014-10-02 22:46:20 +0100

    fix(workflow): Replacing grunt tasks with node modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,24 +6,12 @@ module.exports = function(grunt) {
     var sh = require('shelljs');
 
     grunt.loadNpmTasks('grunt-conventional-changelog');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-ngdocs');
 
-    /*
-        Project configuration
-        According to https://github.com/gruntjs/grunt-contrib-jshint/pull/24#issuecomment-14976842
-        jshint options could not be merged with .jshintrc
-        so we need to use
-
-        options: grunt.util._.merge({}, grunt.file.readJSON('.jshintrc')),
-
-        to merge the required options
-
-    */
     grunt.initConfig({
         docsSever: {
             port: 9001,
@@ -31,68 +19,6 @@ module.exports = function(grunt) {
         clean: {
             docs: {
                 src: ['docs']
-            }
-        },
-        jshint: {
-            server: {
-                /*
-                   TODO add option "devel: true" for production linting
-
-                   This option defines globals that are usually used for
-                   logging poor-man's debugging: console, alert, etc.
-                   It is usually a good idea to not ship them in production because,
-                   for example, console.log breaks in legacy versions of Internet Explorer.
-
-                   http://www.jshint.com/docs/options/#devel
-                 */
-                options: grunt.util._.merge(
-                    grunt.file.readJSON('.jshintrc'),
-                    {
-                        ignores: [
-                            'lib/client/system/libs/*',
-                            'lib/client/system/modules/*',
-                            'lib/websocket/transports/engineio/*',
-                            'test/helpers/connect.js',
-                            'test/fixtures/**'
-                        ]
-                    }
-                ),
-                files: {
-                    src: [
-                        'Gruntfile.js',
-                        'test/**/*.js',
-                        'lib/**/*.js',
-                        'lib/websocket/transports/engineio/index.js'
-                    ]
-                }
-            },
-            client: {
-                options: grunt.util._.merge(
-                    grunt.file.readJSON('.jshintrc'),
-                    {
-                        browser: true,
-                        node   : false,
-                        ignores: [
-                            'lib/client/system/libs/*',
-                            'lib/client/system/index.js'
-                        ]
-                    }
-                ),
-                files: {
-                    src: [
-                        'lib/client/system/**/*.js'
-                    ]
-                }
-            },
-            test: {
-                options: {
-                    ignores: ['test/fixtures']
-                },
-                files: {
-                    src: [
-                        'test/unit/**/*.js'
-                    ]
-                }
             }
         },
         ngdocs: {
@@ -303,8 +229,7 @@ module.exports = function(grunt) {
         });
     });
 
-    grunt.registerTask('default', 'Default task which runs all the required subtasks', ['before-test', 'test']);
-    grunt.registerTask('before-test', ['enforce'/*, 'jshint'*/]); // 'jshint' should be uncommented after we linted all the source code
+    grunt.registerTask('default', 'Default task which runs all the required subtasks', []);
     grunt.registerTask('build:docs', 'Build documentation', ['clean:docs', 'ngdocs']);
     grunt.registerTask('watch:docs', 'Watching for changes and re-building docs', ['concurrent:docsSite']);
     grunt.registerTask('update:docs', 'Update gh-page branch by merging from master', ['shell:update-gh-pages']);
