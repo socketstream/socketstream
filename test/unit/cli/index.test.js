6c898ab48d67991d6909750a8f04ea3fbac01f2f
Paul Jensen <paulbjensen@gmail.com>
2014-10-12 20:04:24 +0100

    test(tests): Added some tests in progress

--- test/unit/cli/index.test.js
+++ test/unit/cli/index.test.js
@@ -2,11 +2,50 @@
 
 
 
+// Dependencies
+//
+// var Gently = require('gently');
+
+
+
 describe('lib/cli/index.js', function () {
 
 
 
-  it('should pass the command line arguments to the generator');
+  describe('#process', function () {
+
+
+
+    describe('when the 1st argument is n', function () {
+
+      it('should call the generator with the program arguments');
+      // For this, we stub the generator using Gently, and assert
+      // that it receives the expected arguments from the CLI module        
+
+    });
+
+
+
+    describe('when the 1st argument is new', function () {
+
+      it('should call the generator with the program arguments');
+      // For this, we stub the generator using Gently, and assert
+      // that it receives the expected arguments from the CLI module        
+
+    });
+
+
+
+    describe('when the 1st argument is neither "n" or "new"', function () {
+
+      it('should inform the user on how to use the application');
+      // NOTE - how do we capture this use case?
+
+    });
+
+
+
+  });
 
 
 
