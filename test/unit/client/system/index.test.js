c0bea3c1c3a8bf43d84bd1176dfccfd0d0715165
Henrik Vendelbo <thepian@thepia.com>
2014-10-25 20:53:12 +0200

    system assets override
    support override of the system assets using send with same file name

--- test/unit/client/system/index.test.js
+++ test/unit/client/system/index.test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var path = require('path'),
+    should = require('should'),
+    ss = require( path.join(process.env.PWD, 'lib/socketstream') );
 
 
 describe('client system library', function () {
@@ -7,9 +10,43 @@ describe('client system library', function () {
 
 
     describe('#send', function () {
+        beforeEach(function() {
 
+            ss.client.assets.unload();
+            ss.client.assets.load();
+        });
 
+        it('should extend shims',function() {
 
+            var jsBefore = ss.client.assets.serve.js();
+            ss.client.assets.send('shim','extra.js','var extra = 0;');
+            var jsAfter = ss.client.assets.serve.js();
+            jsAfter.should.have.length(jsBefore.length + 1 + 14);
+        });
+
+        it('should replace shims',function() {
+
+            var jsBefore = ss.client.assets.serve.js();
+            ss.client.assets.send('shim','json.min.js','');
+            var jsAfter = ss.client.assets.serve.js();
+            jsAfter.should.have.length(jsBefore.length - 1886);
+        });
+
+        it('should extend libs',function() {
+
+            var jsBefore = ss.client.assets.serve.js();
+            ss.client.assets.send('lib','extra.js','var extra = 0;');
+            var jsAfter = ss.client.assets.serve.js();
+            jsAfter.should.have.length(jsBefore.length + 1 + 14);
+        });
+
+        it('should replace libs',function() {
+
+            var jsBefore = ss.client.assets.serve.js();
+            ss.client.assets.send('lib','browserify.js','');
+            var jsAfter = ss.client.assets.serve.js();
+            jsAfter.should.have.length(jsBefore.length - 8854);
+        });
     });
 
 
