6fc300fd5aa674a285310031ad05fe9dd4290e9f
wylie <joshua.cullick@gmail.com>
2015-01-13 17:21:58 +0200

    ...

---
+++ node_modules/engine.io/node_modules/mocha/test.js
@@ -0,0 +1,15 @@
+
+describe('something', function(){
+  it('should work', function(){
+    var a = { test: /something/i };
+    var b = { test: 'hey' }
+    a.should.eql(b)
+  })
+
+  // it('be pending')
+  // it('be pending')
+
+  it('should fail', function(){
+    // tasdf
+  })
+})
