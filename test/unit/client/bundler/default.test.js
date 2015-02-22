3d1b463f196b773e6558e92a305cff551095c7f7
Henrik Vendelbo <henrik@thepia.com>
2015-02-22 16:07:17 +0100

    dummy unit test file

---
+++ test/unit/client/bundler/default.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+describe('default bundler', function () {
+
+
+
+  describe('#entries', function () {
+
+
+
+    it('should return entries for everything needed in view');
+
+
+
+    it('should return be affected by includes flags');
+
+
+
+  });
+
+
+
+});
