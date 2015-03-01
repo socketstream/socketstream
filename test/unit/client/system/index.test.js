148369b63074dc6c825b6622afa95088a21185ea
Henrik Vendelbo <henrik@thepia.com>
2015-03-01 01:33:04 +0100

    fix(tests): system assets

--- test/unit/client/system/index.test.js
+++ test/unit/client/system/index.test.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var path    = require('path'),
-    ss      = require( path.join(process.env.PWD, 'lib/socketstream') );
+    ss      = require( path.join(process.env.PWD, 'lib/socketstream'));
 
 
 describe('client system library', function () {
-
 
 
     describe('#send', function () {
@@ -15,44 +14,29 @@ describe('client system library', function () {
             ss.client.assets.load();
         });
 
-        it('should extend shims',function() {
-
-            var jsBefore, jsAfter;
-
-            jsBefore = ss.client.assets.serve.js();
-            ss.client.assets.send('shim','extra.js','var extra = 0;');
-            jsAfter = ss.client.assets.serve.js();
-            jsAfter.should.have.length(jsBefore.length + 1 + 14);
-        });
-
-        it('should replace shims',function() {
-
-            var jsBefore, jsAfter;
-
-            jsBefore = ss.client.assets.serve.js();
-            ss.client.assets.send('shim','json.min.js','');
-            jsAfter = ss.client.assets.serve.js();
-            jsAfter.should.have.length(jsBefore.length - 1886);
-        });
-
         it('should extend libs',function() {
             
             var jsBefore, jsAfter;
 
-            jsBefore = ss.client.assets.serve.js();
+            jsBefore = ss.api.bundler.systemLibs();
+            jsBefore.should.be.type('object');
+            jsBefore.type.should.be.equal('loader');
             ss.client.assets.send('lib','extra.js','var extra = 0;');
-            jsAfter = ss.client.assets.serve.js();
-            jsAfter.should.have.length(jsBefore.length + 1 + 14);
+            jsAfter = ss.api.bundler.systemLibs();
+            jsAfter.should.be.type('object');
+            jsAfter.content.should.have.length(jsBefore.content.length + 1 + 14);
         });
 
         it('should replace libs',function() {
 
             var jsBefore, jsAfter;
 
-            jsBefore = ss.client.assets.serve.js();
+            jsBefore = ss.api.bundler.systemLibs();
+            jsBefore.should.be.type('object');
+            jsBefore.type.should.be.equal('loader');
             ss.client.assets.send('lib','browserify.js','');
-            jsAfter = ss.client.assets.serve.js();
-            jsAfter.should.have.length(jsBefore.length - 8854);
+            jsAfter = ss.api.bundler.systemLibs();
+            jsAfter.content.should.have.length(jsBefore.content.length - 8854);
         });
     });
 
