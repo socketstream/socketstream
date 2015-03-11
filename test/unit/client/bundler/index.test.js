db71b0586af4fd2f327457904d48df65a17843b7
Henrik Vendelbo <henrik@thepia.com>
2015-03-11 23:27:11 +0100

    test(bundler): basic stuff

--- test/unit/client/bundler/index.test.js
+++ test/unit/client/bundler/index.test.js
@@ -13,6 +13,10 @@ describe('bundler', function () {
 
   describe('API',function() {
 
+    afterEach(function() {
+      ss.client.forget();
+    });
+
     it('should resolve paths in default dirs',function() {
       var paths = ss.api.bundler.sourcePaths({
         css: 'main.css',
@@ -24,5 +28,30 @@ describe('bundler', function () {
       paths.code.should.eql(['./code/main.js']);
       paths.tmpl.should.eql(['./templates/main.html']);
     });
+
+    it('should return bundler given id', function() {
+      var client = ss.client.define('abc', { view: 'abc.html' });
+
+      var bundler = ss.api.bundler.get({ client: 'abc' });
+      bundler.should.be.type('object');
+      bundler.client.should.equal(client);
+    });
+
+    it('should look up bundler by id', function() {
+      var client = ss.client.define('abc2', { view: 'abc.html' });
+
+      var bundler = ss.api.bundler.get({ ts: client.id });
+      bundler.should.be.type('object');
+      bundler.client.should.equal(client);
+    });
+
+    it('should throw odd bundler lookups', function() {
+      var client = ss.client.define('abc3', { view: 'abc.html' });
+
+      should(function() {
+        ss.api.bundler.get({ ts: 'abc' });
+      }).throw(Error);
+    });
   });
+
 });
