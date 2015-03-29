91ddaa4d4106b54e8dae3ee14c2fbe32cbea3f7b
Henrik Vendelbo <henrik@thepia.com>
2015-03-29 19:48:55 +0200

    fix(serve): serve system module

--- test/unit/client/serve/dev.test.js
+++ test/unit/client/serve/dev.test.js
@@ -1,12 +1,49 @@
 'use strict';
 
+var path    = require('path'),
+  should  = require('should'),
+  ss      = require( '../../../../lib/socketstream'),
+  Router = require('../../../../lib/http/router').Router,
+  options = ss.client.options,
+  defineAbcClient = require('../abcClient');
+
+var responseStub = {
+  writeHead: function(status,headers) {},
+  end: function(body) {}
+};
 
 
 describe('development mode asset server', function () {
 
+  var router = new Router();
 
 
-    it('should provide a route for serving system libraries and modules');
+  beforeEach(function() {
+
+    // back to initial client state
+    ss.client.assets.unload();
+    ss.client.assets.load();
+  });
+
+  afterEach(function() {
+    ss.client.forget();
+  });
+
+  it('should serve system loader module',function() {
+
+    var client = defineAbcClient({
+      code: './abc/index.a'
+    }, function() {
+      require('../../../../lib/client/serve/dev')(ss.api, router, options);
+    });
+
+    // dev time URL
+    var req = {url: '/assets/abc/'+client.id+'.js?mod=loader' };
+    router.route(req.url,req,responseStub).should.equal(true);
+    //TODO check that it's the right content
+  });
+
+  it('should provide a route for serving system libraries and modules');
 
 
 
