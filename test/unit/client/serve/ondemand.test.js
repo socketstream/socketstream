1a5c91f408712a9ceccbe216cd092a26d2309ef8
Henrik Vendelbo <henrik@thepia.com>
2015-04-06 09:07:09 +0200

    fix(worker): fixed serving worker files

--- test/unit/client/serve/ondemand.test.js
+++ test/unit/client/serve/ondemand.test.js
@@ -1,16 +1,65 @@
 'use strict';
 
+var path    = require('path'),
+  should  = require('should'),
+  ss      = require( '../../../../lib/socketstream'),
+  Router = require('../../../../lib/http/router').Router,
+  options = ss.client.options,
+  defineAbcClient = require('../abcClient');
+
+var responseStub = {
+  writeHead: function(status,headers) {},
+  end: function(body) {
+    this.body = body;
+  }
+};
 
 
 describe('production mode asset server', function () {
 
+  var router = new Router();
 
 
-    it('should provide a route for serving javascript code');
+  beforeEach(function() {
+
+    // back to initial client state
+    ss.client.assets.unload();
+    ss.client.assets.load();
+    ss.client.formatters.add('javascript');
+  });
+
+  afterEach(function() {
+    ss.client.forget();
+  });
 
 
+  /*TODO
+  it('should provide a route for serving javascript code', function(){
+    code: './abc/index.a'
+  }, function() {
+    require('../../../../lib/client/serve/ondemand')(ss.api, router, options);
+  });
 
-    it('should provide a route for serving web worker requests');
+  // dev time URL
+  var req = {url: '/assets/abc/'+client.id+'.js?mod=loader' };
+  router.route(req.url,req,responseStub).should.equal(true);
+   responseStub.body.should.equal('// calc pi\n');
+  });
+*/
+
+
+    it('should provide a route for serving web worker requests', function() {
+      var client = defineAbcClient({
+        code: './abc/index.a'
+      }, function() {
+        require('../../../../lib/client/serve/ondemand')(ss.api, router, options);
+      });
+
+      // dev time URL
+      var req = {url: '/_serve/worker?/pi.js' };
+      router.route(req.url,req,responseStub).should.equal(true);
+      responseStub.body.should.equal('// calc pi\n');
+    });
 
 
 
