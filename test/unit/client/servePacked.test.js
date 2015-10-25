c1fc962fc00327882d9c319a3400dafa170ed6f3
Henrik Vendelbo <henrik@thepia.com>
2015-10-25 22:32:21 +0100

    fix(servePacked): serve the existing packed assets in either production configs

---
+++ test/unit/client/servePacked.test.js
@@ -0,0 +1,10 @@
+describe('packed client',function() {
+
+  it('should be served when option servePacked or packAssets set');
+
+  it('should serve JS ondemand within bounds defined');
+  
+  it('should serve worker JS in definitions');
+
+  it('should include libs in client');
+});
