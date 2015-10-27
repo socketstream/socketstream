5f495051e7e079d4d85f43d142e12f8e9b616c78
Henrik Vendelbo <henrik@thepia.com>
2015-10-27 22:56:45 +0100

    fixed test for reworked responders

--- test/unit/request/index.test.js
+++ test/unit/request/index.test.js
@@ -1,132 +1,75 @@
 'use strict';
 
+var path = require('path'),
+    ss = require( '../../fixtures/socketstream' ),
+    chai = require('chai'),
+    expect = chai.expect;
 
 
-// Dependencies
-//
-var assert  = require('assert');
-var path    = require('path');
-var index   = require('../../../lib/request/index');
+describe('request responder API', function () {
 
-
-
-describe('index()', function () {
-
-  it('should return an object with add, load, and clear functions', function (done) {
-
-    var ss          = {root:'/some/path'};
-    var loadedIndex = index(ss);
-    assert(typeof loadedIndex === 'object');
-    assert(typeof loadedIndex.add === 'function');
-    assert(typeof loadedIndex.load === 'function');
-    assert(typeof loadedIndex.clear === 'function');
-    done();
-
+  beforeEach(function() {
+    // ss.reset();
   });
 
-  describe('clear()', function () {
-
-    it('should set default middleware to false, so that they are not loaded', function (done) {
-
-      var ss          = {root:'/some/path'};
-      var loadedIndex = index(ss);
-      assert.equal(false,loadedIndex.clear());
-      assert.deepEqual(loadedIndex.load(), {});
-      done();
-
-    });
-
+  it('should return an object with add, load, and clear functions', function () {
+    expect(ss.responders).to.be.an('object');
+    expect(ss.responders.add).to.be.an('function');
+    expect(ss.responders.load).to.be.an('function');
+    expect(ss.responders.clear).to.be.an('function');
   });
 
-  describe('load()', function () {
-
-    it('should load the events and rpc responders by default', function (done) {
-
-      var ss          = {root: path.join(__dirname,'../../../','new_project'), client: {send: function () {}}};
-      var loadedIndex = index(ss);
-      var fullLoad    = loadedIndex.load();
-      assert.deepEqual(fullLoad['0'].name, 'events');
-      assert.deepEqual(fullLoad['1'].name, 'rpc');
-      assert(typeof fullLoad['0'].interfaces.websocket === 'function');
-      assert(typeof fullLoad['1'].interfaces.websocket === 'function');
-      assert(typeof fullLoad['1'].interfaces.internal === 'function');
-      done();
-
-    });
-
+  it('should set default middleware to false, so that they are not loaded', function () {
+    expect(ss.responders.clear()).to.equal(false);
   });
 
-  describe('add()', function () {
-
-    describe('when given a name of a request responder', function () {
-
-      it('should load that request responder from inside of SocketStream\'s responders folder', function (done) {
-
-        var ss          = {root: path.join(__dirname,'../../../','new_project'), client: {send: function () {}}};
-        var loadedIndex = index(ss);
-        var fullLoad    = loadedIndex.add('rpc');
-        assert.equal(fullLoad.name,'rpc');
-        assert(typeof fullLoad.interfaces === 'function');
-        done();
-
-      });
-
-    });
-
-    describe('when given a name of a request responder that does not exist', function () {
-
-      it('should throw an error', function (done) {
-
-        var ss          = {root: path.join(__dirname,'../../../','new_project'), client: {send: function () {}}};
-        var loadedIndex = index(ss);
-        assert.throws(function () {
-          loadedIndex.add('webrtc');
-        });
-        done();
-
-      });
-
-    });
-
-    describe('when given a function', function () {
-
-      it('should load the function as a request responder', function (done) {
-
-        var responderFunc = function () {
-          return {
-            name: 'customResponder',
-            interfaces: function () {}
-          }
-        };
-        var ss          = {root: path.join(__dirname,'../../../','new_project'), client: {send: function () {}}};
-        var loadedIndex = index(ss);
-        loadedIndex.clear();
-        loadedIndex.add(responderFunc);
-        var fullLoad = loadedIndex.load();
-        assert.equal('customResponder',fullLoad['1'].name);
-        done();
-
-      });
-
-    });
-
-    describe('when given a function that has an error when invoked', function () {
-
-      it('should throw an error', function (done) {
-
-        var responderFunc = function () { throw new Error('An error occurred loading the responder'); };
-        var ss          = {root: path.join(__dirname,'../../../','new_project'), client: {send: function () {}}};
-        var loadedIndex = index(ss);
-        loadedIndex.clear();
-        assert.throws(function () {
-          loadedIndex.add(responderFunc);
-        });
-        done();
-
-      });
-
-    });
-
+  it('should load the events and rpc responders by default', function () {
+    var responders = ss.responders.load();
+    expect(responders['0'].name).to.equal('events');
+    expect(responders['1'].name).to.equal('rpc');
+    expect(responders['0'].interfaces.websocket).to.be.a('function');
+    expect(responders['1'].interfaces.websocket).to.be.a('function');
+    expect(responders['1'].interfaces.internal).to.be.a('function');
   });
 
+  describe('when given a name of a request responder', function () {
+
+    it('should load that request responder from inside of responders folder', function () {
+      var loaded = ss.responders.add('rpc');
+      expect(loaded.name).to.equal('rpc');
+      expect(loaded.interfaces).to.be.a('function');
+    });
+
+    it('should throw an error if name unknown', function() {
+      expect(function () {
+        ss.responders.add('webrtc');
+      }).to.throw();
+    });
+  });
+
+  describe('when given a function for a request responder', function () {
+
+    it('should load the request responder', function () {
+      function responderFunc() {
+        return {
+          name: 'customResponder',
+          interfaces: function () {}
+        }
+      }
+      ss.responders.clear();
+      ss.responders.add(responderFunc);
+      var fullLoad = ss.responders.load();
+      expect(fullLoad['6'].name).to.equal('customResponder'); // perhaps this could be a better behaviour just re-adding 'rpc'
+    });
+
+    it('should throw an error if constructor does', function () {
+      function responderFunc() {
+        throw new Error('An error occurred loading the responder');
+      }
+      ss.responders.clear();
+      expect(function() {
+        ss.responders.add(responderFunc);
+      }).to.throw();
+    });
+  });
 });
