00953fd3cf7d120a04f69779b1d9933f067e6268
Henrik Vendelbo <henrik@thepia.com>
2015-10-28 09:04:36 +0100

    chore(engineio): integrated Pauls clientside reconnect tweaks

---
+++ test/unit/websocket/transports/engineio/wrapper.test.js
@@ -0,0 +1,21 @@
+describe("wrapper", function(){
+
+  describe("#connect", function(){
+
+    describe("open event", function(){
+
+      it("should retrieve the session id from the connect.sid cookie");
+      // We need a way to simulate the browser
+      // We need to set a cookie on the browser
+      // We need to load the javascript into that browser's page
+      // We need to run the script which attempts to connect to the server
+
+      it("should send a message back to the server with the session id, if a session id is present");
+
+      it("should raise an error if if is not able to obtain a connect session id");
+
+    });
+
+  });
+
+});
