127b84fba5450ee6479c348b6f20ae37d0bbe26a
Alexander <streamcode9@gmail.com>
2022-03-09 21:49:14 +0100

    test: fix require test

--- test/unit/require.test.js
+++ test/unit/require.test.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var ss = require( '../fixtures/socketstream');
+var should = require('should');
 
 
 describe('ss.require', function() {
 	it('should load builtin', function() {
-		var mod = ss.require('default','client/bundler');
-		expect(mod).should.be(require('../../../lib/client/bundler/default'));
+		var mod = ss.api.require('default','client/bundler');
+		should(mod).equal(require('../../lib/client/bundler/default'));
 	});
 });
